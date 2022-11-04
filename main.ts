a80432c92ddf6dd61d5b063b5ed3cf160824bc5f
raythegae <115674655+raythegae@users.noreply.github.com>
2022-11-04 09:14:37 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,36 @@
+let x = 0
+let y = 0
+input.onButtonPressed(Button.A, function () {
+    x = 0
+    y = 0
+    for (let index = 0; index < 5; index++) {
+        for (let index = 0; index < 5; index++) {
+            led.plot(x, y)
+            x += 1
+            basic.pause(200)
+        }
+        y += 1
+        x = 0
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    x = 0
+    y = 0
+    for (let index = 0; index < 3; index++) {
+        for (let index = 0; index < 5; index++) {
+            led.plot(x, y)
+            x += 1
+            basic.pause(200)
+        }
+        y += 1
+        for (let index = 0; index < 5; index++) {
+            x += -1
+            led.plot(x, y)
+            basic.pause(200)
+        }
+        y += 1
+    }
+})
+basic.forever(function () {
+	
+})
